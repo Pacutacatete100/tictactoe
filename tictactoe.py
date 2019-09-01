3c17417f5b4796dee6af2b6c7169a7d7828a3177
Pacutacatete100 <pacutacatete100@gmail.com>
2019-09-01 02:18:20 -0500

    made separate functions for each small grid

--- tictactoe.py
+++ tictactoe.py
@@ -18,37 +18,107 @@ background.fill((250, 250, 250))
 
 # declare our support functions
 def draw_big_grid():
-    pygame.draw.line(background, (0, 0, 0), (300, 20), (300, 880), 4)
-    pygame.draw.line(background, (0, 0, 0), (600, 20), (600, 880), 4)
+    pygame.draw.line(background, (130, 0, 0), (300, 20), (300, 880), 4)
+    pygame.draw.line(background, (130, 0, 0), (600, 20), (600, 880), 4)
 
     # horizontal lines...
-    pygame.draw.line(background, (0, 0, 0), (20, 300), (880, 300), 4)
-    pygame.draw.line(background, (0, 0, 0), (20, 600), (880, 600), 4)
+    pygame.draw.line(background, (130, 0, 0), (20, 300), (880, 300), 4)
+    pygame.draw.line(background, (130, 0, 0), (20, 600), (880, 600), 4)
 
 def draw_small_grid():
 
-    # horizontal line for small grid
+   draw_small_grid_1_1()
+   draw_small_grid_1_2()
+   draw_small_grid_1_3()
+
+   draw_small_grid_2_1()
+   draw_small_grid_2_2()
+   draw_small_grid_2_3()
+
+   draw_small_grid_3_1()
+   draw_small_grid_3_2()
+   draw_small_grid_3_3()
+
+def draw_small_grid_1_1():
+    # horizontal lines for small grid 1,1
     pygame.draw.line(background, (0, 0, 0), (20, 100), (280, 100), 4)
     pygame.draw.line(background, (0, 0, 0), (20, 200), (280, 200), 4)
-
-    #vertical line for small grid
+    # vertical lines for small grid 1,1
     pygame.draw.line(background, (0, 0, 0), (100, 20), (100, 280), 4)
     pygame.draw.line(background, (0, 0, 0), (200, 20), (200, 280), 4)
 
-    """"
-    -TODO:
-        -make way to call this method in each big square instead of drawing it each time
-            *maybe instead of set numbers use variables to represent certain positions on the big board
-    """
+def draw_small_grid_2_1():
+    # horizontal lines for small grid 2,1
+    pygame.draw.line(background, (0, 0, 0), (320, 100), (580, 100), 4)
+    pygame.draw.line(background, (0, 0, 0), (320, 200), (580, 200), 4)
+    # vertical lines for small grid 2, 1
+    pygame.draw.line(background, (0, 0, 0), (400, 20), (400, 280), 4)
+    pygame.draw.line(background, (0, 0, 0), (500, 20), (500, 280), 4)
 
-def initBoard():
+def draw_small_grid_3_1():
+    # horizontal lines for small grid 3,1
+    pygame.draw.line(background, (0, 0, 0), (620, 100), (880, 100), 4)
+    pygame.draw.line(background, (0, 0, 0), (620, 200), (880, 200), 4)
+    # vertical lines for small grid 3,1
+    pygame.draw.line(background, (0, 0, 0), (700, 20), (700, 280), 4)
+    pygame.draw.line(background, (0, 0, 0), (800, 20), (800, 280), 4)
+
+def draw_small_grid_1_2():
+    # horizontal lines for small grid 1,2
+    pygame.draw.line(background, (0, 0, 0), (20, 400), (280, 400), 4)
+    pygame.draw.line(background, (0, 0, 0), (20, 500), (280, 500), 4)
+    # vertical liens for small grid 1,2
+    pygame.draw.line(background, (0, 0, 0), (100, 320), (100, 580), 4)
+    pygame.draw.line(background, (0, 0, 0), (200, 320), (200, 580), 4)
+
+def draw_small_grid_1_3():
+    # horizontal lines for small grid 1,3
+    pygame.draw.line(background, (0, 0, 0), (20, 700), (280, 700), 4)
+    pygame.draw.line(background, (0, 0, 0), (20, 800), (280, 800), 4)
+    # vertical lines for small grid 1,3
+    pygame.draw.line(background, (0, 0, 0), (100, 620), (100, 880), 4)
+    pygame.draw.line(background, (0, 0, 0), (200, 620), (200, 880), 4)
+
+def draw_small_grid_2_2():
+    # horizontal lines for small grid 2,2
+    pygame.draw.line(background, (0, 0, 0), (320, 400), (580, 400), 4)
+    pygame.draw.line(background, (0, 0, 0), (320, 500), (580, 500), 4)
+    # veritcal lines for small grid 2,2
+    pygame.draw.line(background, (0, 0, 0), (400, 320), (400, 580), 4)
+    pygame.draw.line(background, (0, 0, 0), (500, 320), (500, 580), 4)
+
+def draw_small_grid_3_2():
+    # horizontal lines for small grid 3,2
+    pygame.draw.line(background, (0, 0, 0), (620, 400), (880, 400), 4)
+    pygame.draw.line(background, (0, 0, 0), (620, 500), (880, 500), 4)
+    # vertical lines for small grid 3,2
+    pygame.draw.line(background, (0, 0, 0), (700, 320), (700, 580), 4)
+    pygame.draw.line(background, (0, 0, 0), (800, 320), (800, 580), 4)
+
+def draw_small_grid_3_3():
+    # horizontal lines for small grid 3,3
+    pygame.draw.line(background, (0, 0, 0), (620, 700), (880, 700), 4)
+    pygame.draw.line(background, (0, 0, 0), (620, 800), (880, 800), 4)
+    # vertical liens fro small grid 3,3
+    pygame.draw.line(background, (0, 0, 0), (700, 620), (700, 880), 4)
+    pygame.draw.line(background, (0, 0, 0), (800, 620), (800, 880), 4)
+
+def draw_small_grid_2_3():
+    # horizontal lines for small grid 2,3
+    pygame.draw.line(background, (0, 0, 0), (320, 700), (580, 700), 4)
+    pygame.draw.line(background, (0, 0, 0), (320, 800), (580, 800), 4)
+    # vertical lines for small grid 2,3
+    pygame.draw.line(background, (0, 0, 0), (400, 620), (400, 880), 4)
+    pygame.draw.line(background, (0, 0, 0), (500, 620), (500, 880), 4)
+
+def init_board():
 
     draw_big_grid()
     draw_small_grid()
 
     return background
 
-def drawStatus(board):
+def draw_status(board):
     # draw the status (i.e., player turn, etc) at the bottom of the board
     # ---------------------------------------------------------------
     # board : the initialized game board surface where the status will
@@ -72,18 +142,18 @@ def drawStatus(board):
     board.blit(text, (10, 880))
 
 
-def showBoard(window_size, board):
+def show_board(window_size, board):
     # redraw the game board on the display
     # ---------------------------------------------------------------
     # window_size   : the initialized pyGame display
     # board : the game board surface
 
-    drawStatus(board)
+    draw_status(board)
     window_size.blit(board, (0, 0))
     pygame.display.flip()
 
 
-def boardPos(mouseX, mouseY):
+def board_position(mouseX, mouseY):
     # given a set of coordinates from the mouse, determine which board space
     # (row, column) the user clicked in.
     # ---------------------------------------------------------------
@@ -110,7 +180,7 @@ def boardPos(mouseX, mouseY):
     return (row, col)
 
 
-def drawMove(board, boardRow, boardCol, Piece):
+def draw_move(board, boardRow, boardCol, Piece):
     # draw an X or O (Piece) on the board in boardRow, boardCol
     # ---------------------------------------------------------------
     # board     : the game board surface
@@ -135,7 +205,7 @@ def drawMove(board, boardRow, boardCol, Piece):
     grid[boardRow][boardCol] = Piece
 
 
-def clickBoard(board):
+def click_board(board):
     # determine where the user clicked and if the space is not already
     # occupied, draw the appropriate piece there (X or O)
     # ---------------------------------------------------------------
@@ -144,7 +214,7 @@ def clickBoard(board):
     global grid, XO
 
     (mouseX, mouseY) = pygame.mouse.get_pos()
-    (row, col) = boardPos(mouseX, mouseY)
+    (row, col) = board_position(mouseX, mouseY)
 
     # make sure no one's used this space
     if ((grid[row][col] == "X") or (grid[row][col] == "O")):
@@ -152,7 +222,7 @@ def clickBoard(board):
         return
 
     # draw an X or O
-    drawMove(board, row, col, XO)
+    draw_move(board, row, col, XO)
 
     # toggle XO to the other player's move
     if (XO == "X"):
@@ -161,7 +231,7 @@ def clickBoard(board):
         XO = "X"
 
 
-def gameWon(board):
+def check_for_win(board):
     # determine if anyone has won the game
     # ---------------------------------------------------------------
     # board : the game board surface
@@ -175,7 +245,7 @@ def gameWon(board):
             # this row won
             winner = grid[row][0]
             pygame.draw.line(board, (250, 0, 0), (0, (row + 1) * 100 - 50),
-                             (300, (row + 1) * 100 - 50), 2)
+                             (300, (row + 1) * 100 - 50), 4)
             break
 
     # check for winning columns
@@ -185,7 +255,7 @@ def gameWon(board):
             # this column won
             winner = grid[0][col]
             pygame.draw.line(board, (250, 0, 0), ((col + 1) * 100 - 50, 0),
-                             ((col + 1) * 100 - 50, 300), 2)
+                             ((col + 1) * 100 - 50, 300), 4)
             break
 
     # check for diagonal winners
@@ -193,13 +263,13 @@ def gameWon(board):
             (grid[0][0] is not None):
         # game won diagonally left to right
         winner = grid[0][0]
-        pygame.draw.line(board, (250, 0, 0), (50, 50), (250, 250), 2)
+        pygame.draw.line(board, (250, 0, 0), (50, 50), (250, 250), 4)
 
     if (grid[0][2] == grid[1][1] == grid[2][0]) and \
             (grid[0][2] is not None):
         # game won diagonally right to left
         winner = grid[0][2]
-        pygame.draw.line(board, (250, 0, 0), (250, 50), (50, 250), 2)
+        pygame.draw.line(board, (250, 0, 0), (250, 50), (50, 250), 4)
 
 
 # --------------------------------------------------------------------
@@ -208,7 +278,7 @@ pygame.init()
 pygame.display.set_caption('Tic-Tac-Toe')
 
 # create the game board
-board = initBoard()
+board = init_board()
 
 # main event loop
 running = 1
@@ -219,10 +289,10 @@ while (running == 1):
             running = 0
         elif event.type is MOUSEBUTTONDOWN:
             # the user clicked; place an X or O
-            clickBoard(board)
+            click_board(board)
 
         # check for a winner
-        gameWon(board)
+        check_for_win(board)
 
         # update the display
-        showBoard(window_size, board)
+        show_board(window_size, board)
